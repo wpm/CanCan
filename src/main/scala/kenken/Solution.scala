d557cf029274018bf1edf0e1d5affb25518ca5d8
W.P. McNeill <billmcn@gmail.com>
2012-10-12 16:21:16 -0700

    Implement Solution as a Vector of Vectors

--- src/main/scala/kenken/Solution.scala
+++ src/main/scala/kenken/Solution.scala
@@ -1,23 +1,23 @@
 package kenken
 
-import scala.Array
-
 /**
  * A solution to a KenKen problem.
  *
  * A solution is an assignment of values to cells. Solutions may be partial or complete. Solutions are always valid.
  * @param values array of values in the solution
  */
-case class Solution private (values: Array[Array[Option[Int]]]) {
-  def this(n:Int) = this(Array.ofDim[Option[Int]](n,n))
+case class Solution private (values: Vector[Vector[Option[Int]]]) {
+  def this(n:Int) = this(Vector.fill[Option[Int]](n,n)(null))
+
+  def addHypothesis(h:Map[(Int, Int), Int]) = {
+    val a = values.toArray.map(_.toArray)
+    for (cell <- h.keys) a(cell._1)(cell._2) = Some(h(cell))
+    Solution(Vector(a:_*).map(Vector(_:_*)))
+  }
 
   def isComplete = !values.flatten.contains(null)
 
   def apply(cell:(Int, Int)) = values(cell._1)(cell._2)
-
-  def update(cell:(Int, Int), value:Option[Int]) {values(cell._1)(cell._2) = value}
-
-  def update(cell:(Int, Int), value:Int) {update(cell, Some(value))}
 
   override def toString = values.map(_.map(v=>if (v == null) "." else v.get).mkString(" ")).mkString("\n")
 }
