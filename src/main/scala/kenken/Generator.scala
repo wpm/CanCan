3a053a2055774e2190428f7ad5e210dd52c0d33f
W.P. McNeill <billmcn@gmail.com>
2012-11-08 20:30:11 -0800

    Generate random constraints from solution and cages

--- src/main/scala/kenken/Generator.scala
+++ src/main/scala/kenken/Generator.scala
@@ -7,13 +7,54 @@ import scala.util.Random._
  * Generator of random KenKen puzzles
  */
 object Generator {
-  def randomLatinSquare(n: Int): List[List[Int]] = {
+  /**
+   * Generate a random KenKen puzzle and its solution
+   * @param n puzzle size
+   * @return (solution, puzzle) tuple
+   */
+  def randomPuzzle(n: Int): (Array[Array[Int]], KenKen) = {
+    val solution = randomLatinSquare(n)
+    val cages = randomCageLayout(n)
+    (solution, KenKen(n, cages.map(cage => randomCageConstraint(solution, cage.toList))))
+  }
+
+  /**
+   * Generate a random cage constraint from a cage and a solution grid
+   */
+  def randomCageConstraint(solution: Array[Array[Int]], cage: List[(Int, Int)]): Constraint = {
+    def randomAssociativeConstraint(values: List[Int]) = {
+      nextInt(2) match {
+        case 0 => PlusConstraint(values.sum, cage)
+        case _ => TimesConstraint(values.product, cage)
+      }
+    }
+
+    def randomNonAssociativeConstraint(values: List[Int]) = {
+      require(values.size == 2)
+      val (smaller, larger) = (values.sorted.head, values.sorted.last)
+      require(smaller != larger)
+      shuffle(MinusConstraint(larger - smaller, cage.head, cage.last) ::
+        (if (larger % smaller == 0) List(DivideConstraint(larger / smaller, cage.head, cage.last)) else Nil)).head
+    }
+
+    val values = cage.map {
+      case (r, c) => solution(r - 1)(c - 1)
+    }
+    cage.size match {
+      case 1 => SpecifiedConstraint(values.head, cage.head)
+      // 2-cell cages choose a non-associative constraint 2/3s of the time
+      case 2 => if (nextInt(3) == 1) randomAssociativeConstraint(values) else randomNonAssociativeConstraint(values)
+      case _ => randomAssociativeConstraint(values)
+    }
+  }
+
+  def randomLatinSquare(n: Int): Array[Array[Int]] = {
     def rotate[E](xs: List[E]) = (xs.head :: xs.tail.reverse).reverse
     // Write a random permutation in the first row, generate successive rows
     // by rotation, then shuffle the rows.
     shuffle((List(shuffle((1 to n).toList)) /: (1 to n - 1)) {
       case (rows, _) => rotate(rows.head) :: rows
-    })
+    }).map(_.toArray).toArray
   }
 
   /**
@@ -97,6 +138,7 @@ object Generator {
   }
 
   def main(args: Array[String]) {
+    def latinSquareToString(s: Array[Array[Int]]) = s.map(_.mkString(" ")).mkString("\n")
     def adjacent[N](edges: Map[N, List[N]])(n: N): List[N] = edges.getOrElse(n, Nil)
     val edges = Map('a -> List('b, 'c), 'b -> List('a, 'c), 'c -> List('a, 'b),
       'd -> List('e), 'e -> List('d), 'f -> Nil)
@@ -104,8 +146,10 @@ object Generator {
     val cs = connectedComponents(nodes, adjacent(edges)(_: Symbol))
     println(cs)
 
-    println(randomLatinSquare(9).mkString("\n"))
+    println(latinSquareToString(randomLatinSquare(9)))
 
-    println(randomCageLayout(6))
+    val (solution, puzzle) = randomPuzzle(6)
+    println(latinSquareToString(solution))
+    println(puzzle)
   }
 }
